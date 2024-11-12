eb829f6f3ba0967e20e3df9755710a816f7fd1ab
shadiya <118953383+shadiyaali@users.noreply.github.com>
2024-11-12 15:43:11 +0530

    first

--- src/components/mobile-menu/MobileMenuSection.jsx
+++ src/components/mobile-menu/MobileMenuSection.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { FarzaaContext } from "../../context/FarzaaContext";
-
+import { Link } from "react-router-dom";
 const MobileMenuSection = ({ navigate }) => {
   const { isDropdownOpen, handleDropdownToggle } = useContext(FarzaaContext);
   return (
@@ -15,31 +15,35 @@ const MobileMenuSection = ({ navigate }) => {
       <nav className="mobile-mean-nav">
         <div className="mobile-menu-list-items">
           <div className="fz-dropdown fz-nav-item">
+          <Link to="/">
             <a role="button" className="fz-nav-link">
               <span>Home</span>
                
             </a>
- 
+            </Link>
           </div>
            
           <div className="fz-dropdown fz-nav-item">
+          <Link to="/shop">
             <a role="button" className="fz-nav-link">
               <span>Products</span>
          
             </a>
-
+            </Link>
             
                
           </div>
           <div className="fz-dropdown fz-nav-item">
+          <Link to="/custom-shop">
             <a role="button" className="fz-nav-link">
               <span>Customized Products</span>
         
             </a>
-
+            </Link>
             
           </div>
           <div className="fz-nav-item mean-last">
+          <Link to="/contact">
             <a
               role="button"
               // onClick={() => navigate("/contact")}
@@ -47,6 +51,7 @@ const MobileMenuSection = ({ navigate }) => {
             >
               Contact
             </a>
+            </Link>
           </div>
         </div>
       </nav>
