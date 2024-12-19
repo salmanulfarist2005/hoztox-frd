cf6f1ff524a703267cd66454464fb86f01a653ce
shadiya <118953383+shadiyaali@users.noreply.github.com>
2024-12-19 16:56:28 +0530

    last

--- src/components/offer/SpecialOfferSection.jsx
+++ src/components/offer/SpecialOfferSection.jsx
@@ -19,7 +19,7 @@ const SpecialOfferSection = () => {
 
                     <div className="col-lg-5 col-6 col-xxs-12 align-self-end">
                         <div className="fz-spcial-offer__img">
-                            <img src="assets/images/ring.png" alt="Ring Image"/>
+                            <img src="https://shop.caratreediamonds.com/assets/images/ring.png" alt="Ring Image"/>
                         </div>
                     </div>
                 </div>
