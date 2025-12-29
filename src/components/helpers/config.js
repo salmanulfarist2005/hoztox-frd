30f4b5adb3d642127a93d0df0a98ad766ed8e025
nithinraj1999 <vnithinraj99@gmail.com>
2025-12-29 14:56:49 +0530

    fix: reset config

--- src/components/helpers/config.js
+++ src/components/helpers/config.js
@@ -1,7 +1,7 @@
 // export const BASE_URL = 'http://127.0.0.1:8000'
-//  export const BASE_URL = 'https://hoztox-o4rf.onrender.com' 
+ export const BASE_URL = 'https://hoztox-o4rf.onrender.com' 
 
      // export const BASE_URL = 'https://vnzms23g-8000.inc1.devtunnels.ms/'
 
 
- export const BASE_URL = 'https://1b2f9s23-8000.inc1.devtunnels.ms/' 
+//  export const BASE_URL = 'https://1b2f9s23-8000.inc1.devtunnels.ms/' 
